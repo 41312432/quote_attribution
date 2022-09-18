49685d640429100b27efdef21425486c6c7de59a
Wansik Jo <jws5327@ajou.ac.kr>
2022-09-18 10:01:35 +0000

    fix : feat train for checking

--- utils/arguments.py
+++ utils/arguments.py
@@ -22,21 +22,19 @@ def get_train_args():
     parser.add_argument('--window_size', type=int, default=10)
 
     #model
-    parser.add_argument('--pooling_type', type=str, default='max_pooling')
+    parser.add_argument('--pooling_type', type=str, default='mean_pooling')
     parser.add_argument('--classifier_intermediate_dim', type=int, default=100)
     parser.add_argument('--nonlinear_type', type=str, default='relu')
-
-    parser.add_argument('--bert_pretrained_dir', type=str, default='')
 
     #training
     parser.add_argument('--margin', type=float, default=1.0)
     parser.add_argument('--lr', type=float, default=1e-5)
     parser.add_argument('--optimizer', type=str, default='sgd')
     parser.add_argument('--dropout', type=float, default=0.5)
-    parser.add_argument('--num_epochs', type=int, default=50)
-    parser.add_argument('--batch_size', type=int, default=4)
+    parser.add_argument('--num_epochs', type=int, default=30)
+    parser.add_argument('--batch_size', type=int, default=16)
     parser.add_argument('--lr_decay', type=float, default=0.95)
-    parser.add_argument('--patience', type=int, default=10)
+    parser.add_argument('--patience', type=int, default=5)
 
     args = parser.parse_args()
     return args
